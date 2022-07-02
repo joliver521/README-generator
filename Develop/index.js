3c6c2f065a12f2ab69195ea0a55432ec157f370b
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-02 15:47:06 -0400

    added generateMarkdown variable

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeToFile(fileName, data, 'utf8', function (err) {
+function writetoFile(fileName, data) {
+  fs.writeFile(fileName, data, 'utf8', function (err) {
     if (err) {
       throw err;
     }
