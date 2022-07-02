0db45a4534fe5b084c6cfebb3c494950a097ad62
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-02 15:42:28 -0400

    add fs and inquirer variables

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeToFile(fileName, data, 'utf8', function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log(`You have successfully written your README file`);
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
